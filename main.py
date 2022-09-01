be500276a4c0536326dea557417478984e8aee86
Jazib Hassan <jazibulhassan95@gmail.com>
2022-09-01 20:10:00 +0100

    First Commits

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 import os
+import textract
+
+
 
 file_names = os.listdir("data")
 
+text = textract.process('data/book1.pdf', method='tesseract')
+
+print(text)
+
+# pdfFileObj = open('data/book1.pdf', 'rb')
+#
+# # creating a pdf reader object
+# pdfReader = PyPDF3.PdfFileReader(pdfFileObj)
+#
+# # printing number of pages in pdf file
+# print(pdfReader.numPages)
+#
+# # creating a page object
+# pageObj = pdfReader.getPage(100)
+#
+# # extracting text from page
+# okay = pageObj.extractText()
+# escaped = re.escape(okay)
+# print(okay)
+#
+# # closing the pdf file object
+# pdfFileObj.close()
---
+++ plan.md
@@ -0,0 +1,14 @@
+We are going to break things down into following steps
+
+
+1. Read a pdf and parse text
+2. Separate our paragraphs
+3. Filter/recommend paragraphs that may contain value for us
+
+
+
+
+## Additional Features
+1. Read book from google drive
+2. Automatically post on group
+3. Take input on topics to filter on
